c3a935f1c63b5b6857780da21745eb7cc7ca1ae9
Bilguun13 <83194126+Bilguun13@users.noreply.github.com>
2022-05-14 15:15:21 +0800

    main functionality

--- lib/main.rb
+++ lib/main.rb
@@ -1,88 +1,61 @@
 class Game
   attr_accessor :board
   def initialize
-    @board = Array.new(6) { Array.new(7) { 'x' } }
+    @board = Array.new(6) { Array.new(7) { '🟦' } }
   end
 
   def clear
-    @board = Array.new(6) { Array.new(7) { 'x' } }
+    @board = Array.new(6) { Array.new(7) { '🟦' } }
   end
 
   def drop_disc(col_no, sign, possible_row = nil)
-    5.downto(0) {|i| possible_row = i and break if board[i][col_no] == 'x' }
+    5.downto(0) { |i| possible_row = i and break if board[i][col_no] == '🟦' }
     possible_row.nil? ? (puts 'This column is full') : (@board[possible_row][col_no] = sign)
   end
 
-  def four_in_row_horizontally?(current_pos, sign, queue = [], no_of_success = 0, history = [])
-    # implemented using BFS
-    queue.push(current_pos)
-    until queue.empty? || no_of_success == 4
-      history.push(current = queue.shift)
-      left = [current[0], current[1] - 1]
-      right = [current[0], current[1] + 1]
-      no_of_success += 1
-      # adding left node if possible
-      queue.push(left) if is_valid?(left, history, sign)
-      queue.push(right) if is_valid?(right, history, sign)
-    end
-    no_of_success.eql?(4) ? true : false
-  end
-
-  def four_in_row_diagnolly?(current_pos, sign, queue = [], no_of_success = 0, history = [])
-    # implemented using BFS
-    queue.push(current_pos)
-    until queue.empty? || no_of_success == 4
-      history.push(current = queue.shift)
-      left = [current[0] + 1, current[1] - 1]
-      right = [current[0] - 1, current[1] + 1]
-      no_of_success += 1
-      # adding left node if possible
-      queue.push(left) if is_valid?(left, history, sign)
-      queue.push(right) if is_valid?(right, history, sign)
-    end
-    no_of_success.eql?(4) ? true : false
-  end
-
-  def four_in_row_vertically?(current_pos, sign, queue = [], no_of_success = 0, history = [])
-    # implemented using BFS
-    queue.push(current_pos)
-    until queue.empty? || no_of_success == 4
-      history.push(current = queue.shift)
-      # in this case, left would be up, right would be down
-      left = [current[0] - 1, current[1]]
-      right = [current[0] + 1, current[1]]
-      no_of_success += 1
-      # adding left node if possible
-      queue.push(left) if is_valid?(left, history, sign)
-      queue.push(right) if is_valid?(right, history, sign)
-    end
-    no_of_success.eql?(4) ? true : false
-  end
-
-  def four_in_row(current_pos, sign, direction, queue = [], no_of_success = 0, history = [])
+  def four_in_row?(current_pos, sign, direction, queue = [], no_of_success = 0, history = [])
     # implemented using BFS
     queue.push(current_pos)
     until queue.empty? || no_of_success == 4
       history.push(current = queue.shift)
       no_of_success += 1
-      left = [current[0], current[1] - 1] and right = [current[0], current[1] + 1] if direction == 'x'
-      left = [current[0] + 1, current[1] - 1] and right = [current[0] - 1, current[1] + 1] if direction == 'z'
-      left = [current[0] - 1, current[1]] and right = [current[0] + 1, current[1]] if direction == 'y'
-      # adding left and right node if possible
-      queue.push(left) if is_valid?(left, history, sign)
-      queue.push(right) if is_valid?(right, history, sign)
+      search(queue, current, direction, history, sign)
     end
     no_of_success.eql?(4) ? true : false
   end
 
-  def is_valid?(pos, history, sign)
+  def search(queue, current, direction, history, sign)
+    left = [current[0], current[1] - 1] and right = [current[0], current[1] + 1] if direction == 'x'
+    left = [current[0] + 1, current[1] - 1] and right = [current[0] - 1, current[1] + 1] if direction == 'z'
+    left = [current[0] - 1, current[1]] and right = [current[0] + 1, current[1]] if direction == 'y'
+    # adding left and right node if possible
+    queue.push(left) if valid?(left, history, sign)
+    queue.push(right) if valid?(right, history, sign)
+  end
+
+  def valid?(pos, history, sign)
     # also the sign/disc should match
     return false unless pos[0].between?(0,5) and pos[1].between?(0,5)
     return false unless @board[pos[0]][pos[1]] == sign
     return false if history.include?(pos)
+
     true
+  end
+
+  def display_board
+    for i in 0..5
+      for j in 0..6
+        print board[i][j]
+      end
+      puts ""
+    end
+  end
+
+  def start_game
+
   end
 end
 
 # misc: 🟡 , 🔴
 game = Game.new
+game.display_board
--- spec/connect4_spec.rb
+++ spec/connect4_spec.rb
@@ -4,7 +4,7 @@ describe Game do
   game = Game.new
   describe ".initialize" do
     it "initializes new game with the 6x7 game board(array)" do 
-      expect(game.board).to eql( Array.new(6) {Array.new(7) {'x'}} )
+      expect(game.board).to eql( Array.new(6) {Array.new(7) {'🟦'}} )
     end
   end
 
@@ -32,59 +32,76 @@ describe Game do
 
   describe "#clear" do
     it "refreshes the game board" do
-      expect(game.clear).to eql( Array.new(6) {Array.new(7) {'x'}} )
+      expect(game.clear).to eql( Array.new(6) {Array.new(7) {'🟦'}} )
     end
   end
 
-  describe "#is_valid?" do
+  describe "#valid?" do
     it "checks if child node of the current position is available to add to queue" do
       game.drop_disc(0, 'd')
       game.drop_disc(1, 'd')
       game.drop_disc(2, 'd')
       game.drop_disc(3, 'd')
-      expect(game.is_valid?([5,1], [1,1], 'd')).to eql(true)
-      expect(game.is_valid?([5,4],[[5,1], [1,1]], 'd')).to eql(false)
-      expect(game.is_valid?([5,3],[[5,3], [1,1]], 'd')).to eql(false)
+      expect(game.valid?([5,1], [1,1], 'd')).to eql(true)
+      expect(game.valid?([5,4],[[5,1], [1,1]], 'd')).to eql(false)
+      expect(game.valid?([5,3],[[5,3], [1,1]], 'd')).to eql(false)
     end
   end
 
-  describe "#four_in_row_horizontally?" do
+  describe "#four_in_row?" do
     it "Searches if there are four discs in row horizontally" do
       game.clear
       game.drop_disc(0, 'd')
       game.drop_disc(1, 'd')
       game.drop_disc(2, 'd')
       game.drop_disc(3, 'd')
-      expect(game.four_in_row_horizontally?([5,1], 'd')).to eql(true)
-      expect(game.four_in_row_horizontally?([5,0], 'd')).to eql(true)
-      expect(game.four_in_row_horizontally?([5,3], 'd')).to eql(true)
-      expect(game.four_in_row_horizontally?([5,2], 'd')).to eql(true)
+      expect(game.four_in_row?([5,1], 'd', 'x')).to eql(true)
+      expect(game.four_in_row?([5,0], 'd', 'x')).to eql(true)
+      expect(game.four_in_row?([5,3], 'd', 'x')).to eql(true)
+      expect(game.four_in_row?([5,2], 'd', 'x')).to eql(true)
     end
   end
 
-  describe "#four_in_row_diagnolly?" do
-    it "Searches if there are four discs in row horizontally" do
+  describe "#four_in_row?" do
+    it "Searches if there are four discs in row diagnolly" do
       game.clear
       game.drop_disc(0, 'd')
       2.times {game.drop_disc(1, 'd')}
       3.times {game.drop_disc(2, 'd')}
       4.times {game.drop_disc(3, 'd')}
-      expect(game.four_in_row_diagnolly?([5,0], 'd')).to eql(true)
-      expect(game.four_in_row_diagnolly?([5,1], 'd')).to eql(false)
-      expect(game.four_in_row_diagnolly?([4,1], 'd')).to eql(true)
-      expect(game.four_in_row_diagnolly?([2,3], 'd')).to eql(true)
+      expect(game.four_in_row?([5,0], 'd', 'z')).to eql(true)
+      expect(game.four_in_row?([5,1], 'd', 'z')).to eql(false)
+      expect(game.four_in_row?([4,1], 'd', 'z')).to eql(true)
+      expect(game.four_in_row?([2,3], 'd', 'z')).to eql(true)
     end
   end
 
 
-  describe "#four_in_row_vertically?" do
-    it "Searches if there are four discs in row horizontally" do
+  describe "#four_in_row?" do
+    it "Searches if there are four discs in row vertically" do
       game.clear
       4.times {game.drop_disc(3, 'd')}
-      expect(game.four_in_row_vertically?([5,3], 'd')).to eql(true)
-      expect(game.four_in_row_vertically?([2,3], 'd')).to eql(true)
-      expect(game.four_in_row_vertically?([4,3], 'd')).to eql(true)
-      expect(game.four_in_row_vertically?([2,4], 'd')).to eql(false)
+      expect(game.four_in_row?([5,3], 'd', 'y')).to eql(true)
+      expect(game.four_in_row?([2,3], 'd', 'y')).to eql(true)
+      expect(game.four_in_row?([4,3], 'd', 'y')).to eql(true)
+      expect(game.four_in_row?([2,4], 'd', 'y')).to eql(false)
     end
   end
+=begin
+  describe "#display_board" do
+    it "displays the board in a aesthetic way!" do
+      game.clear
+      4.times {game.drop_disc(3, '🔴')}
+      expect(game.display_board).to
+      eql([['🟦', '🟦', '🟦', '🟦', '🟦', '🟦', '🟦'],
+           ['🟦', '🟦', '🟦', '🟦', '🟦', '🟦', '🟦'],
+           ['🟦', '🟦', '🟦', '🔴', '🟦', '🟦', '🟦'],
+           ['🟦', '🟦', '🟦', '🔴', '🟦', '🟦', '🟦'],
+           ['🟦', '🟦', '🟦', '🔴', '🟦', '🟦', '🟦'],
+           ['🟦', '🟦', '🟦', '🔴', '🟦', '🟦', '🟦']]
+       )
+    end
+  end
+=end
+
 end
